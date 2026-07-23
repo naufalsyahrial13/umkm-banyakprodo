c0767bf615c5451547bd42c46a4eebb941ed9e2e
naufalsyahrial13 <naufalsyahrial13@gmail.com>
2026-07-23 13:00:40 +0700

    Update site url untuk Open Graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://umkm-banyakprodo.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   }
